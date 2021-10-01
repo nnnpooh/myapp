3647e10fd125cc7dca508cd63c12c6666ccd6508
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2021-10-01 22:47:28 +0700

    add ability to delete and update data

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ function App() {
   const [course, setCourses] = useState([]);
   const [courseName, setCourseName] = useState('');
   const [courseNumber, setcourseNumber] = useState('');
+  const [courseId, setCourseId] = useState(0);
+  const [editMode, setEditMode] = useState(false);
 
   async function loadData() {
     let { data } = await supabase.from('course').select('*');
@@ -44,13 +46,54 @@ function App() {
     }
   }
 
+  async function handleDelete(id) {
+    console.log(id);
+
+    const { data, error } = await supabase.from('course').delete().eq('id', id);
+
+    await loadData();
+  }
+
+  async function handleEdit(id) {
+    setEditMode(true);
+    let { data: course, error } = await supabase.from('course').select('*');
+
+    const courseData = course[0];
+
+    setCourseName(courseData.course_name);
+    setcourseNumber(courseData.course_number);
+    setCourseId(courseData.id);
+  }
+
+  async function submitEditCourseForm() {
+    console.log(courseId);
+    const { data, error } = await supabase
+      .from('course')
+      .update({ course_name: courseName, course_number: courseNumber })
+      .eq('id', courseId);
+
+    await loadData();
+
+    setCourseName('');
+    setcourseNumber('');
+    setEditMode(false);
+  }
+
   return (
     <div className='App'>
       <div>
         <h1>Course List</h1>
         {course.map((course) => (
           <li key={course.id}>
-            {course.course_name}, {course.course_number}
+            {course.course_name}, {course.course_number}{' '}
+            <span
+              onClick={() => {
+                handleDelete(course.id);
+              }}
+            >
+              [X]
+            </span>
+            <span onClick={() => handleEdit(course.id)}>[Edit]</span>
           </li>
         ))}
       </div>
@@ -59,26 +102,32 @@ function App() {
         <h1>Add Course</h1>
 
         <form onSubmit={submitCourseForm}>
-          <label htmlFor='course_name'>Course Name</label>
-          <input
-            id='course_name'
-            value={courseName}
-            onChange={handleCourseName}
-            type='text'
-          ></input>
-          <label htmlFor='course_number'>Course Number</label>
-          <input
-            id='course_number'
-            type='text'
-            value={courseNumber}
-            onChange={handleCourseNumber}
-          ></input>
-          <button type='submit'>Add Course</button>
+          <div>
+            <label htmlFor='course_name'>Course Name</label>
+            <input
+              id='course_name'
+              value={courseName}
+              onChange={handleCourseName}
+              type='text'
+            ></input>
+          </div>
+          <div>
+            <label htmlFor='course_number'>Course Number</label>
+            <input
+              id='course_number'
+              type='text'
+              value={courseNumber}
+              onChange={handleCourseNumber}
+            ></input>
+          </div>
+          {!editMode && <button type='submit'>Add Course</button>}
         </form>
+        {editMode && (
+          <button onClick={submitEditCourseForm}>Edit Course</button>
+        )}
       </div>
     </div>
   );
 }
 
 export default App;
-
